99b78e4f6c9b504d22256791a5e7d70ec48b9e93
72tommy72 <hatem27slama78@gamil.com>
2024-07-31 18:02:56 +0300

    asds

--- src/appRouter.js
+++ src/appRouter.js
@@ -5,55 +5,75 @@ import userInfoRouter from './modules/userInfo/userInfo.router.js'
 import morgan from 'morgan';
 
 export const appRouter = (app, express) => {
-    //morgan
-    if (process.env.NODE_ENV === "dev") {
-        app.use(morgan("dev"));
-    } else {
-        app.use(morgan("combined"));
-    }
-    // //cors
-    // const whiteList = [];
-    // app.use((req, res, next) => {
-    //     //activation account
-    //     if (req.originalUrl.includes('/auth/confirmEmail')) {
-    //         res.setHeader('Access-Control-Allow-Origin', "*");
-    //         res.setHeader('Access-Control-Allow-Headers', "GET");
-    //         return next();
-    //     }
-    //     if (!whiteList.includes(req.header('origin'))) {
-    //         return next(new Error('blocked by cors'));
-    //     }
-    //     res.setHeader('Access-Control-Allow-Origin', "*");
-    //     res.setHeader('Access-Control-Allow-Headers', "*");
-    //     res.setHeader('Access-Control-Allow-Methods', "*");
-    //     res.setHeader('Access-Control-Allow-Private-Network', true);
-    //     return next();
-    // })
-    //global Routes
-    app.use(express.json()) // parse data from cover to Json
+//morgan
+if (process.env.NODE_ENV === "dev") {
+    app.use(morgan("dev"));
+} else {
+    app.use(morgan("combined"));
+}
+// //cors
+// const whiteList = [];
+// app.use((req, res, next) => {
+//     //activation account
+//     if (req.originalUrl.includes('/auth/confirmEmail')) {
+//         res.setHeader('Access-Control-Allow-Origin', "*");
+//         res.setHeader('Access-Control-Allow-Headers', "GET");
+//         return next();
+//     }
+//     if (!whiteList.includes(req.header('origin'))) {
+//         return next(new Error('blocked by cors'));
+//     }
+//     res.setHeader('Access-Control-Allow-Origin', "*");
+//     res.setHeader('Access-Control-Allow-Headers', "*");
+//     res.setHeader('Access-Control-Allow-Methods', "*");
+//     res.setHeader('Access-Control-Allow-Private-Network', true);
+//     return next();
+// })
+// Middleware to enable CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,Patch ,PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
-    //APIs for user
-    app.use('/auth', authRouter)
+//global Routes
+app.use(express.json()) // parse data from cover to Json
+
+//APIs for user
+app.use('/auth', authRouter)
     //APIs for imageAn
-    app.use('/model', imageAnRouter)
+app.use('/model', imageAnRouter)
     //APIs for userChanges
-    app.use('/userChanges', userChangesRouter)
+app.use('/userChanges', userChangesRouter)
     //APIs for userInfoRouter
-    app.use('/', userInfoRouter)
+app.use('/', userInfoRouter)
 
 
-    // Not Found Page 
-    app.use('/*', (req, res, next) => {
-        return next(new Error('Not Found', { cause: 404 }));
+// Not Found Page 
+app.use('/*', (req, res, next) => {
+    return next(new Error('Not Found', { cause: 404 }));
+})
+
+//error handler
+app.use((error, req, res, next) => {
+    return res.status(error.cause || 500).json({
+        success: false,
+        message: error.message,
+        stack: error.stack
     })
+})
 
-    //error handler
-    app.use((error, req, res, next) => {
-        return res.status(error.cause || 500).json({
-            success: false,
-            message: error.message,
-            stack: error.stack
-        })
+}
+})
+
+//error handler
+app.use((error, req, res, next) => {
+    return res.status(error.cause || 500).json({
+        success: false,
+        message: error.message,
+        stack: error.stack
     })
+})
 
 }

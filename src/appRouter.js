10f3e2bf7b11fd6ed8f4ea6188955f2d5d178559
72tommy72 <hatem27slama78@gamil.com>
2024-08-03 01:09:30 +0300

    2

--- src/appRouter.js
+++ src/appRouter.js
@@ -31,7 +31,7 @@ export const appRouter = (app, express) => {
     // })
     // Middleware to enable CORS
     app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST,Patch ,PUT, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         next();
